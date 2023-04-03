24758e2fbea31e8e8d1a4ce1091305d05fa0d42d
tpflaher <117224459+tpflaher@users.noreply.github.com>
2023-04-03 11:41:36 -0400

    Added mapping of the throttle
    also got rid of exponential cahnge

--- SSE_Electronic_Throttle_Only.ino
+++ SSE_Electronic_Throttle_Only.ino
@@ -3,18 +3,35 @@
 Servo throttle;   // Create a Servo object for the throttle
 int potpin = A0;  // Connect the potentiometer to pin A0
 int val;          // Variable to store the potentiometer value
+unsigned long prevtime;
 
 void setup() {
-  throttle.attach(9);  // Attach the servo to pin 9
-  pinMode(potpin, INPUT);  // Set the potentiometer pin as an input
+ 
+  pinMode(potpin, INPUT); // Set the potentiometer pin as an input
+pinMode(9,OUTPUT);  
+  Serial.begin(9600); 
+  prevtime = millis();
 }
 
-void loop() {
-  val = analogRead(potpin);  // Read the potentiometer value
-  val = map(val, 0, 1023, 0, 180);  // Convert the value to a range of 0-180 (throttle range)
-  // Exponential rate of change
-  val = pow(val, 2) / (180 * 180); // makes the values exponential and then puts it back into the range
-  throttle.write(val);  // Set the throttle position
+void loop() { 
+  throttle.attach(9);  // Attach the servo to pin 9
+  val = analogRead(potpin);
+  if(val>230){  // Read the potentiometer value
+    val = map(val, 230, 879, 50, 106);  // Convert the value to a range of 0-180 (throttle range)
+     //Exponential rate of change
+    //val = pow(val, 2) / (180 * 180); // makes the values exponential and then puts it back into the range
+     throttle.write(val);
+    // Set the throttle position
+  } else{
+    val=50;
+    throttle.write(val);
+ } 
+   if( millis()- prevtime>= 100){
+   Serial.println(val);
+    prevtime = millis();
+ }  
 }
   
+
+
 
